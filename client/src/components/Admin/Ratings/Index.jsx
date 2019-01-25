d83025a8fd5511d7e8c0d693529d9f0952aa8bec
SoniaRMK <soniakxxx@gmail.com>
2019-01-25 10:07:34 +0300

    feat(viewRatings): admin view ratings
    - implement the UI to enable an admin view all ratings of meals served for the day
    
    [Starts #158378175]

--- client/src/components/Admin/Ratings/Index.jsx
+++ client/src/components/Admin/Ratings/Index.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import DatePicker from 'react-date-picker';
+import moment from 'moment';
 
 import RatingsTabs from './RatingsTabs';
 
@@ -22,56 +23,58 @@ class Ratings extends Component {
 
 	render() {
 		const { isOpen, end } = this.state;
+		const today = moment().format('dddd, MMMM Do YYYY');
 
 		return (
 			<Fragment>
 				<div>
 					<div className="title">
-			            <span>Ratings for Monday, 26th November 2018</span>
-			            <div className="filter">
-			              <button
-			                className="button"
-			                type="button"
-			                onClick={this.handleFilterModal}
-			              ><i className="fas fa-filter" />   Filter
-			              </button>
-			              <form
-			                className={`dropdown ${isOpen && "active"}`}
-			              >
-			                <div>
-			                  <label className="date-label" htmlFor="end">End Date
-			                    <DatePicker
-			                      onChange={(date) => this.setState({ end: date })}
-			                      value={end}
-			                    />
-			                  </label>
-			                </div>
-			                <button
-			                  className="input btn"
-			                  type="button"
-			                  onClick={this.handleFilterSubmit}
-			                >
-			                  Submit
-			                </button>
-			                <div className="actions">
-			                  <a
-			                    className="action-item"
-			                    role="button"
-			                    tabIndex="0"
-			                    onClick={() => this.setState({ end: '' })}
-			                  >Clear filters
-			                  </a>
-			                  <a
-			                    className="action-item"
-			                    role="button"
-			                    tabIndex="0"
-			                    onClick={this.handleFilterModal}
-			                  >Close
-			                  </a>
-			                </div>
-			              </form>
-			            </div>
-			          </div>
+						<span className="rating-title">Ratings for {today}</span>
+						<br/><br/><br/>
+						<div className="filter">
+							<button
+								className="button"
+								type="button"
+								onClick={this.handleFilterModal}
+							><i className="fas fa-filter" />   Filter
+							</button>
+							<form
+								className={`dropdown ${isOpen && "active"}`}
+							>
+								<div>
+									<label className="date-label" htmlFor="end">End Date
+										<DatePicker
+											onChange={(date) => this.setState({ end: date })}
+											value={end}
+										/>
+									</label>
+								</div>
+								<button
+									className="input btn"
+									type="button"
+									onClick={this.handleFilterSubmit}
+								>
+									Submit
+								</button>
+								<div className="actions">
+									<a
+										className="action-item"
+										role="button"
+										tabIndex="0"
+										onClick={() => this.setState({ end: '' })}
+									>Clear filters
+									</a>
+									<a
+										className="action-item"
+										role="button"
+										tabIndex="0"
+										onClick={this.handleFilterModal}
+									>Close
+									</a>
+								</div>
+							</form>
+						</div>
+					</div>
 				</div>
 				<RatingsTabs />
 			</Fragment>
--- client/src/components/Admin/Ratings/RatingsTabs.jsx
+++ client/src/components/Admin/Ratings/RatingsTabs.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Tabs from '../../common/Tab/Index';
+import ReactStars from 'react-stars';
+import { userRatings } from '../../../store/Ratings';
 
 
-const RatingsTabs = () => (
-	<div>
-    <Tabs>
-      <div label="Rice">
-        <h1>Rice</h1>
+class RatingsTabs extends Component {
+  render() {
+    let results = [userRatings.result];
+    let me = "jksdkdk";
+    console.log(userRatings);
+    
+    return (
+      <div>
+      
+          <Tabs>
+            <div label={me}>
+            </div>
+            <div label="ghjk">
+            </div>
+          </Tabs>
+        <div className="rating-container" label="Rice">
+          <h1 className="rating-sub-title">Main Meal Rated: {userRatings.result[0].mainMeal}</h1>
+          <h1 className="vendor-name rating-sub-title">Vendor: {userRatings.vendor}</h1>
+          <hr className="line-style" />
+          <span><br />Overall Rating:</span>
+          <ReactStars
+            value={userRatings.result[0].overallRating}
+            color2="green"
+            name="ratings"
+            size={12}
+            edit={false}
+            />
+          <br /><br /><br />
+          <span><strong>Comments</strong></span><br/><br/>
+          <div className="comments-row">
+            <span>Awesome!</span><br /><br />
+            <ReactStars
+            value={4}
+            color2="green"
+            name="ratings"
+            size={12}
+            edit={false}
+            />
+          </div>
+        </div>
       </div>
-      <div label="Swallow">
-        <h1>Swallow</h1>
-      </div>
-      <div label="Beans">
-        <h1>Beans</h1>
-      </div>
-    </Tabs>
-  </div>
-)
+    );
+  }
+}
 
 export default RatingsTabs;
