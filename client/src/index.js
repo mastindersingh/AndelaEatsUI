8469d072ce1046d1a4c0947938265c19a5610f62
Ayoola Ajebeku <ayz4sci@yahoo.com>
2019-02-26 10:39:20 +0100

    Fix code style issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,10 +11,8 @@ import "react-day-picker/lib/style.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 import "./assets/images/favicon.ico";
-import axios from "axios";
 
 // set the application baseUrl
-// axios.defaults.baseURL = "https://andelaeats.herokuapp.com/api/v1";
 axios.defaults.baseURL = process.env.API_BASE_URL
 
 const store = configureStore();
