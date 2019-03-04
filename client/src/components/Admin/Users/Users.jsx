ac1f2220e1a7ed5a6c597e9121ec26ca78681943
SoniaRMK <soniakxxx@gmail.com>
2019-03-04 09:59:18 +0300

    ft(adminUsers): admin should view admin user list
    - create actions and reducers
    - implement UI for the admin users list
    - write tests for the implementation
    
    [Delivers #164296390]

--- client/src/components/Admin/Users/Users.jsx
+++ client/src/components/Admin/Users/Users.jsx
@@ -6,10 +6,14 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 
 // Actions
-import { createAdminUser } from '../../../actions/admin/adminUserAction';
+import { createAdminUser, getAllAdminUsers } from '../../../actions/admin/adminUserAction';
 
 
 export class Users extends Component {
+
+  componentDidMount() {
+    this.props.getAllAdminUsers();
+	}
 
   handleSubmit = (event) => {
     event.preventDefault();
@@ -21,11 +25,11 @@ export class Users extends Component {
   }
 
   render() {
+    const { adminUsers } = this.props;
 
     return (
       <div>
         <span className="heading-style">Assign Admin role to a user</span>
-        <hr className="line"/>
         <form className="parent-div" onSubmit={this.handleSubmit}>
           <label htmlFor="userEmail">Email</label>
           <input  className="user-form" type="text" id="userEmail" name="userEmail"/>
@@ -33,6 +37,38 @@ export class Users extends Component {
             Assign Admin Role
           </button>
         </form><br/><br/>
+        <span className="heading-style">List of Admin Users</span><br/><br/>
+        {
+          adminUsers.length <= 0 && (
+            <Loader />
+          )
+        }
+
+        {
+          adminUsers.length > 0 && (
+          <div className="table-header custom-row">
+            <div className="custom-col-5">Name</div>
+            <div className="custom-col-5">Email</div>
+            <div className="custom-col-2">Options</div>
+          </div>)
+        }
+
+        {
+          adminUsers.length > 0 && (
+            adminUsers.map((adminUser, key) => (
+              <div  key = {key} className="table-body">
+                <div className="table-row">
+                  <div className="custom-row">
+                    <div className="custom-col-5">{adminUser.Name}</div>
+                    <div className="custom-col-5">{adminUser.Email}</div>
+                    <div className="custom-col-2">
+                      <span className="delete-color">Revoke</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )))
+        }
         <ToastContainer />
       </div>
     );
@@ -42,13 +78,15 @@ export class Users extends Component {
 function mapStateToProps(state) {
   return {
     userEmail: state.user.email,
-    message: state.user.message
+    message: state.user.message,
+    adminUsers: state.user.adminUsers
   };
 }
 
 Users.propTypes = {
   createAdminUser: PropTypes.func.isRequired,
+  getAllAdminUsers: PropTypes.func.isRequired,
   userEmail: PropTypes.string,
 };
 
-export default connect(mapStateToProps, { createAdminUser })(Users);
+export default connect(mapStateToProps, { createAdminUser, getAllAdminUsers })(Users);
