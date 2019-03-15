a9c642f316f5763bfb91ef8a91805d77ef7977a4
Dominic Motuka <dominic.motuka@gmail.com>
2019-03-15 17:05:25 +0300

    Create env file at repo root

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,21 +1,60 @@
 #!/usr/bin/env bash
-set -eo pipefail
 
-#!/bin/bash
-if [ -e $1 ]; then
-  echo "File $1 already exists!"
-  echo "Removing and  creating a new file"
-  rm ../.env
-else
-  echo >> $1
-fi
+set -o errexit
+set -o pipefail
+
+DIRECTORY="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+ROOT_DIRECTORY=$(dirname $DIRECTORY)
 
 
-echo "CLOUDINARY_CLOUD_NAME=$CLOUDINARY_CLOUD_NAME" >> ../.env
-echo "CLOUDINARY_API_KEY=$CLOUDINARY_API_KEY" >> ../.env
-echo "CLOUDINARY_API_SECRET=$CLOUDINARY_API_SECRET" >> ../.env
-echo "CLOUDINARY_URL=$CLOUDINARY_URL" >> ../.env
-echo "API_BASE_URL=$API_BASE_URL" >> ../.env
-echo "BASE_URL=$BASE_URL" >> ../.env
-echo "SENTRY_URL=$SENTRY_URL" >> ../.env
-echo "ANDELA_API_URL=$ANDELA_API_URL" >> ../.env
+# colors
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+tan=$(tput setaf 3)
+white=$(tput setaf 7)
+
+success() {
+  printf "${green}===> %s${reset}\n" "$@"
+}
+
+error() {
+  printf "${red}===> %s${reset}\n" "$@"
+}
+
+warning() {
+  printf "${tan}===> %s${reset}\n" "$@"
+}
+
+reset() {
+  printf "${white}"
+}
+
+function addEnvFile() {
+  ENV_FILE=$ROOT_DIRECTORY/.env
+  warning "Adding .env file to Andela Easts root project directory"
+  echo " "
+
+  if [ ! -f "$ENV_FILE" ]; then
+    cat <<EOF >>${ROOT_DIRECTORY}/.env
+CLOUDINARY_CLOUD_NAME=${CLOUDINARY_CLOUD_NAME}
+CLOUDINARY_API_KEY=${CLOUDINARY_API_KEY}
+CLOUDINARY_API_SECRET=${CLOUDINARY_API_SECRET}
+CLOUDINARY_URL=${CLOUDINARY_URL}
+API_BASE_URL=${API_BASE_URL}
+BASE_URL=${BASE_URL}
+SENTRY_URL=${SENTRY_URL}
+ANDELA_API_URL=${ANDELA_API_URL}
+EOF
+    success "lsEnvironment file has been created successfully"
+    return
+  fi
+
+  warning "Skipping, Environment file already exist"
+  reset
+}
+
+main () {
+  addEnvFile
+}
+main
