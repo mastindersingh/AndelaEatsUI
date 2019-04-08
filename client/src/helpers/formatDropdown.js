2d2231ecbbee3dc6cb58751ef9c1cd7b8118e492
Ayoola Ajebeku <ayz4sci@yahoo.com>
2019-04-08 17:32:26 +0100

    refactor(eslint): Refactored tmeplate string literal out of formatDropdown

--- client/src/helpers/formatDropdown.js
+++ client/src/helpers/formatDropdown.js
@@ -1,11 +1,17 @@
+/* 
+  Disabled prefer-template because 
+  while using template strings, 
+  the code kept breaking the linter. 
+*/
+/* eslint-disable prefer-template */
 const formatDropdown = (engagements = []) => {
   engagements.map(engagement => {
-    const name = engagement.vendor.name;
+    const { name } = engagement.vendor;
     const startDate = engagement.startDate.slice(5, 17);
     const endDate = engagement.endDate.slice(5, 17);
     return {
-      value: engagement.id,
-      label:`${name} - ${startDate} to ${endDate}`
+      value: engagement.id, 
+      label: name + " - " + startDate + " to " + endDate
     };
   });
 };
