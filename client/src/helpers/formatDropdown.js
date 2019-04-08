fb567706aae365b8dad229a2071df91e0f244aa2
Ayoola Ajebeku <ayz4sci@yahoo.com>
2019-04-08 17:50:53 +0100

    fix(menu-modal-test): Fixes failing MenuModal test

--- client/src/helpers/formatDropdown.js
+++ client/src/helpers/formatDropdown.js
@@ -4,16 +4,14 @@
   the code kept breaking the linter. 
 */
 /* eslint-disable prefer-template */
-const formatDropdown = (engagements = []) => {
-  engagements.map(engagement => {
-    const { name } = engagement.vendor;
-    const startDate = engagement.startDate.slice(5, 17);
-    const endDate = engagement.endDate.slice(5, 17);
-    return {
-      value: engagement.id, 
-      label: name + " - " + startDate + " to " + endDate
-    };
-  });
-};
+const formatDropdown = (engagements = []) => engagements.map(engagement => {
+  const { name } = engagement.vendor;
+  const startDate = engagement.startDate.slice(5, 17);
+  const endDate = engagement.endDate.slice(5, 17);
+  return {
+    value: engagement.id,
+    label: name + " - " + startDate + " to " + endDate 
+  };
+});
 
 export default formatDropdown;
